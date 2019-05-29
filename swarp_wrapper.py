58f238fe05b1d06d85c23c5afeb059488a9fbc9b
mriener <riener@mpia.de>
2019-05-29 12:54:46 +0200

    Refactoring.

--- swarp_wrapper.py
+++ swarp_wrapper.py
@@ -8,31 +8,103 @@ import numpy as np
 from astropy.io import fits
 from tqdm import tqdm
 
+
 class Swarp(object):
-    def __init__(self, list_path_to_cubes, settings_file=None):
-        self.list_path_to_cubes = list_path_to_cubes
-        self.settings_file = settings_file
+    def __init__(self):
+        self.path_swarp = None
+        self.list_spectral_cubes = None
+        self.list_images = None
+        self.swarp_configuration_file = None
+        self.filename_final = None
 
         self.max_channels = None
-        self.list_of_channels = []
-
-        self.filename_final = None
 
         self.verbose = True
         self.overwrite = True
-        self.remove_temorary_files = True
+        self.remove_temporary_files = True
+        self.save_weighted_coaddition_map = True
+        self.save_swarp_log = True
+        self.restore_nans = True
 
-        self.path_swarp = None
+    def check_settings(self):
+        if self.path_swarp is None:
+            raise Exception("Need to specify 'path_swarp'")
+        if not os.path.exists(self.path_swarp):
+            os.makedirs(self.path_swarp)
+        if self.swarp_configuration_file is None:
+            raise Exception("Need to specify 'swarp_configuration_file', i.e. the path to the SWarp configuration file.")
+        if (self.list_spectral_cubes is None) and (self.list_images is None):
+            raise Exception("Need to supply either 'list_spectral_cubes' (= list of paths to spectral cubes) or 'list_images' (list of paths to images)")
 
-        self.list_slices = []
-        self.list_of_images = None
-        self.path_images = None
+    def clean_up(self):
+        if self.list_spectral_cubes is not None:
+            path_log_file = os.path.join(self.path_slices, 'swarp.xml')
+            path_weighted_coadditon_map = os.path.join(
+                self.path_slices, 'coadd.weight.fits')
+        elif self.list_images is not None:
+            path_log_file = os.path.join(self.path_swarp, 'swarp.xml')
+            path_weighted_coadditon_map = os.path.join(
+                self.path_swarp, 'coadd.weight.fits')
+
+        if self.save_swarp_log:
+            path_log_file_new = os.path.join(
+                self.path_swarp, '{}.xml'.format(self.filename_final))
+            os.system('mv {} {}'.format(path_log_file, path_log_file_new))
+        else:
+            os.system('rm {}'.format(path_log_file))
+
+        if self.save_weighted_coaddition_map:
+            path_weighted_coadditon_map_new = os.path.join(
+                self.path_swarp, '{}.coadd.weight.fits'.format(
+                    self.filename_final))
+            os.system('mv {} {}'.format(path_weighted_coadditon_map,
+                                        path_weighted_coadditon_map_new))
+        else:
+            os.system('rm {}'.format(path_weighted_coadditon_map))
+
+    def initialize_cube_mosaicking(self):
+        if self.list_spectral_cubes is None:
+            raise Exception("Need to supply either 'list_spectral_cubes' (= list of paths to spectral cubes)")
+
+        self.path_channels = os.path.join(self.path_swarp, 'channels')
+        if not os.path.exists(self.path_channels):
+            os.makedirs(self.path_channels)
+
+        self.path_slices = os.path.join(self.path_swarp, 'slices')
+        if not os.path.exists(self.path_slices):
+            os.makedirs(self.path_slices)
+
+        for channel in range(self.max_channels):
+            self.list_of_channels.append('channel_{:04d}'.format(channel))
+
+    def mosaick_cube(self):
+        self.check_settings()
+        self.initialize_cube_mosaicking()
+        self.max_channels = self.check_channels()
+        self.slice_cubes()
+        self.swarp_slices()
+        self.assemble_cube()
+        self.clean_up()
+
+        if self.remove_temporary_files:
+            os.system('rm -r {}'.format(self.path_slices))
+            os.system('rm -r {}'.format(self.path_channels))
+
+        if self.restore_nans:
+            self.restore_nan_values()
+
+    def mosaick_image(self):
+        self.check_settings()
+        self.assemble_image()
+        self.clean_up()
+        if self.restore_nans:
+            self.restore_nan_values()
 
     def slice_cubes(self):
-        self.check_channels()
+        self.list_of_channels = []
 
-        start = True
-        for path_to_cube in self.list_path_to_cubes:
+        # start = True
+        for path_to_cube in self.list_spectral_cubes:
             self.dirname = os.path.dirname(path_to_cube)
             self.file = os.path.basename(path_to_cube)
             self.filename, self.file_extension = os.path.splitext(self.file)
@@ -47,29 +119,27 @@ class Swarp(object):
             if len(self.data.shape) == 4:
                 self.correct_stokes()
 
-            self.max_channels = self.check_channels()
-
-            if start:
-                start = False
-
-                if self.path_swarp is None:
-                    self.path_swarp = os.path.join(self.dirname, 'swarp_files')
-
-                self.path_channels = os.path.join(self.path_swarp, 'channels')
-                if not os.path.exists(self.path_channels):
-                    os.makedirs(self.path_channels)
-
-                self.path_slices = os.path.join(self.path_swarp, 'slices')
-                if not os.path.exists(self.path_slices):
-                    os.makedirs(self.path_slices)
-
-                for channel in range(self.max_channels):
-                    self.list_of_channels.append(
-                            'channel_{:04d}'.format(channel))
+            # if start:
+            #     start = False
+            #
+            #     if self.path_swarp is None:
+            #         self.path_swarp = os.path.join(self.dirname, 'swarp_files')
+            #
+            #     self.path_channels = os.path.join(self.path_swarp, 'channels')
+            #     if not os.path.exists(self.path_channels):
+            #         os.makedirs(self.path_channels)
+            #
+            #     self.path_slices = os.path.join(self.path_swarp, 'slices')
+            #     if not os.path.exists(self.path_slices):
+            #         os.makedirs(self.path_slices)
+            #
+            #     for channel in range(self.max_channels):
+            #         self.list_of_channels.append(
+            #                 'channel_{:04d}'.format(channel))
 
             for channel in range(self.max_channels):
                 if channel > (self.header['NAXIS3'] - 1):
-                    "create 2D array with nan values here"
+                    # create 2D array with nan values
                     slice_data = np.empty(
                             (self.header['NAXIS2'], self.header['NAXIS1']))
                     slice_data[:] = np.NAN
@@ -98,7 +168,7 @@ class Swarp(object):
 
     def check_channels(self):
         list_channels = []
-        for path_to_cube in self.list_path_to_cubes:
+        for path_to_cube in self.list_spectral_cubes:
             hdu = fits.open(path_to_cube)[0]
             header = hdu.header
             list_channels.append(header['NAXIS3'])
@@ -119,10 +189,10 @@ class Swarp(object):
         if self.verbose:
             print("swarp slices...")
 
+        self.list_slices = []
+
         pbar = tqdm(total=len(self.list_of_channels))
         for channel in self.list_of_channels:
-            # if self.verbose:
-            #     print("slice {}...".format(channel))
             pbar.update(1)
             nameListFiles = os.path.join(
                     self.path_channels, '*{}*'.format(channel))
@@ -131,9 +201,9 @@ class Swarp(object):
             cwd = os.getcwd()
             os.chdir(self.path_slices)
             os.system('swarp {a} -c {b} -IMAGEOUT_NAME {c} -VERBOSE_TYPE QUIET'.format(
-                    a=nameListFiles, b=self.settings_file, c=filename))
+                    a=nameListFiles, b=self.swarp_configuration_file, c=filename))
             os.chdir(cwd)
-            # if self.remove_temorary_files:
+            # if self.remove_temporary_files:
             #     os.system('rm {}'.format(nameListFiles))
         pbar.close()
 
@@ -166,17 +236,6 @@ class Swarp(object):
         fits.writeto(path_to_file, array, header=header,
                      overwrite=self.overwrite)
 
-        os.chdir(self.path_swarp)
-        path_log_file = os.path.join(self.path_slices, 'swarp.xml')
-        path_log_file_new = os.path.join(self.path_swarp, '{}.xml'.format(self.filename_final))
-        os.system('mv {} {}'.format(path_log_file, path_log_file_new))
-
-        if self.remove_temorary_files:
-            os.system('rm -r {}'.format(self.path_slices))
-            os.system('rm -r {}'.format(self.path_channels))
-            # for filename in self.list_slices:
-            #     os.system('rm {}'.format(filename))
-
     def correct_swarp_header(self, header):
         header['NAXIS'] = 3
         header['CTYPE1'] = header['CTYPE1']
@@ -207,17 +266,21 @@ class Swarp(object):
     def assemble_image(self):
         if self.verbose:
             print("assembling final image...")
-        os.chdir(self.path_images)
-        print(self.list_of_images)
-        if isinstance(self.list_of_images, list):
+        os.chdir(self.path_swarp)
+        if self.verbose:
+            print(self.list_images)
+        if isinstance(self.list_images, list):
             string = ''
-            for img in self.list_of_images:
+            for img in self.list_images:
                 string += img + ' '
-            self.list_of_images = string
-        filename = os.path.join(self.path_images, '{}.fits'.format(self.filename_final))
-        print(filename)
-        os.system('swarp {a} -c {b} -IMAGEOUT_NAME {c} -VERBOSE_TYPE QUIET'.format(
-                a=self.list_of_images, b=self.settings_file, c=filename))
+            self.list_images = string
+        filename = os.path.join(
+            self.path_swarp, '{}.fits'.format(self.filename_final))
+        if self.verbose:
+            print(filename)
+        os.system(
+            'swarp {a} -c {b} -IMAGEOUT_NAME {c} -VERBOSE_TYPE QUIET'.format(
+                a=self.list_images, b=self.swarp_configuration_file, c=filename))
 
     def initialize_array(self, header):
         x = header['NAXIS1']
@@ -231,10 +294,7 @@ class Swarp(object):
     def restore_nan_values(self):
         if self.verbose:
             print("restoring nan values...")
-        if self.path_swarp is not None:
-            os.chdir(self.path_swarp)
-        else:
-            os.chdir(self.path_images)
+        os.chdir(self.path_swarp)
 
         hdu = fits.open('{}.fits'.format(self.filename_final))[0]
         data = hdu.data
@@ -242,15 +302,10 @@ class Swarp(object):
 
         data[data < -1e5] = np.nan
 
-        if self.path_swarp is not None:
-            path_to_file = os.path.join(
-                    self.path_swarp, '{}.fits'.format(self.filename_final))
-            if self.verbose:
-                print("saved '{}' in {}".format(self.filename_final, self.path_swarp))
-        else:
-            path_to_file = os.path.join(
-                    self.path_images, '{}.fits'.format(self.filename_final))
-            if self.verbose:
-                print("saved '{}' in {}".format(self.filename_final, self.path_images))
+        path_to_file = os.path.join(
+                self.path_swarp, '{}.fits'.format(self.filename_final))
+        if self.verbose:
+            print("saved '{}' in {}".format(self.filename_final, self.path_swarp))
+
         fits.writeto(path_to_file, data, header=header,
                      overwrite=self.overwrite)
