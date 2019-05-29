00dec554b76a1c21c425e98fe2454336d21c6028
mriener <riener@mpia.de>
2019-05-29 10:34:55 +0200

    Consider empty rows or cols in center of image for astrometry_info.

--- swarp_helper.py
+++ swarp_helper.py
@@ -4,6 +4,17 @@ import numpy as np
 from astropy.wcs import WCS
 from astropy.io import fits
 
+
+def sequence_of_leading_zeros(array):
+    count_zeros = 0
+    for item in array:
+        if item == 0:
+            count_zeros += 1
+        else:
+            break
+    return count_zeros
+
+
 def astrometry_info(filepath):
     data = fits.getdata(filepath)
     header = fits.getheader(filepath)
@@ -11,28 +22,21 @@ def astrometry_info(filepath):
     wcs = WCS(header)
 
     sum_cols = np.sum(data, axis=0)
-    nonzero_cols = np.count_nonzero(sum_cols)
     sum_rows = np.sum(data, axis=1)
-    nonzero_rows = np.count_nonzero(sum_rows)
 
-    x_offset = 0
-    for item in sum_cols:
-        if item == 0:
-            x_offset += 1
-        else:
-            break
+    zero_cols_start = sequence_of_leading_zeros(sum_cols)
+    zero_cols_end = sequence_of_leading_zeros(sum_cols[::-1])
 
-    y_offset = 0
-    for item in sum_rows:
-        if item == 0:
-            y_offset += 1
-        else:
-            break
+    zero_rows_start = sequence_of_leading_zeros(sum_rows)
+    zero_rows_end = sequence_of_leading_zeros(sum_rows[::-1])
 
-    xpix_center = x_offset + (nonzero_cols - 1) / 2
-    ypix_center = y_offset + (nonzero_rows - 1) / 2
+    size_x = sum_cols.size - zero_cols_start - zero_cols_end
+    size_y = sum_rows.size - zero_rows_start - zero_rows_end
+
+    xpix_center = zero_cols_start + (size_x - 1) / 2
+    ypix_center = zero_rows_start + (size_y - 1) / 2
 
     xcoord_center, ycoord_center = wcs.all_pix2world(xpix_center, ypix_center, 0)
 
     print('CENTER {}, {}'.format(xcoord_center, ycoord_center))
-    print('IMAGE_SIZE {}, {}'.format(nonzero_cols, nonzero_rows))
+    print('IMAGE_SIZE {}, {}'.format(size_x, size_y))
